24b6207fe440b96f4e6992c78441ab06f5803b60
Asheesh Laroia <asheesh@asheesh.org>
2009-02-20 14:32:12 -0800

    Refer to correct path for theme-bundled CC by image

--- ahab.php
+++ ahab.php
@@ -135,7 +135,7 @@ class AhabTemplate extends QuickTemplate {
 			<div id="copyleft">
 				<a href="http://learn.creativecommons.org" id="cclearn"><img src="/images/common/cclearn_logo.gif" alt="ccLearn" width="92" height="23"/></a>
 				<div id="license">
-					<img src="/images/common/license_cc_by.gif" alt="Creative Commons BY License" width="88" height="31"/>
+					<img src="<?php $this->text('stylepath') ?>/ahab/from_whitewhale/images/common/license_cc_by.gif" alt="Creative Commons BY License" width="88" height="31"/>
 					<p>Except where otherwise noted, content on this site is licensed under a <a href="#">Creative Commons Attribution 3.0 License</a>.</p>
 				</div>
 			</div>
