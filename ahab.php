33c49e5c8b4d3d2b8b24ea3348d64999eff35c6b
Asheesh Laroia <asheesh@asheesh.org>
2009-03-02 15:24:45 -0800

    Add a half-hearted attempt to migrate the Main_Page to ahabskin

--- ahab.php
+++ ahab.php
@@ -401,3 +401,83 @@ class AhabTemplate extends QuickTemplate {
 } // end of class
 
 
+class AhabFrontPageTemplate extends AhabTemplate {
+	function execute() {
+		global $wgRequest;
+		$this->skin = $skin = $this->data['sk1in'];
+		$action = $wgRequest->getText('action');
+		
+		// Suppress warnings to prevent notices about missing
+		// indexes in $this->data
+		
+		wfSuppressWarnings();
+
+		?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+	<title>Open Ed at Creative Commons</title>
+		<style type="text/css" media="screen, projection">/*<![CDATA[*/
+			@import "<?php $this->text('stylepath') ?>/ahab/from_whitewhale/styles/opened.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";
+			@import "<?php $this->text('stylepath') ?>/ahab/from_whitewhale/styles/homepage.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";
+		/*]]>*/</style>
+	<script type="text/javascript" src="scripts/beforeload.js"></script>
+</head>
+<body>
+<a href="#content" class="skiplink">Skip to content</a>
+<div id="clip">
+	<div id="container">
+		<div id="frame">
+			<div id="header">
+				<h1><a href="/"><img src="images/common/opened_logo_tagline.gif" alt="Open Ed: The Open Education Project at Creative Commons" width="214" height="173"/></a></h1>
+				<form id="search" action="/search/" method="get">
+					<label for="search_query" id="search_title">Search Open Ed</label>
+					<input name="q" id="search_query" type="text"/><input type="submit" id="search_submit" value="Go"/>
+					<ul id="search_sources">
+						<li><input type="checkbox" id="search_oer" checked="checked"/><label for="search_oer">Open ed (DiscoverED)</label>
+						<li><input type="checkbox" id="search_odepo" checked="checked"/><label for="search_odepo">Organizations (ODEPO)</label>
+						<li><input type="checkbox" id="search_community" checked="checked"/><label for="search_community">Community resources</label>
+						<li><input type="checkbox" id="search_site" checked="checked"/><label for="search_site">This site</label>
+					</ul>
+				</form>
+				<ul id="navigation">
+					<li><a href="/about/">What is Open Education?</a></li>	
+					<li><a href="/teachers/">For Teachers</a></li>
+					<li><a href="/learners/">For Learners</a></li>
+					<li><a href="/community/">For the Open Ed Community</a></li>
+					<li><a href="/license/">License Your Work</a></li>
+					<li><a href="/getinvolved/">Get Involved</a></li>
+				</ul>
+			</div>
+			<div id="content">
+					<?php include 'from_whitewhale/content.slider.php'; ?>
+			</div>
+			<div id="copyleft">
+				<a id="cclearn" href="http://learn.creativecommons.org"><img src="images/common/cclearn_logo.gif" alt="ccLearn" width="92" height="23"/></a>
+				<div id="license">
+					<img src="images/common/license_cc_by.gif" alt="Creative Commons BY License" width="88" height="31"/>
+					<p>Except where otherwise noted, content on this site is licensed under a <a href="#">Creative Commons Attribution 3.0 License</a>.</p>
+				</div>
+			</div>
+		</div>
+	<div id="footer">
+		<ul>
+	        <li id="home"><a href="/">The Open Education Project <span>at</span> Creative Commons</a></li>
+	        <li id="contact"><a href="#">Contact Us</a></li>
+		</ul>
+	</div>
+</div>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-effects.core-1.6rc6.min.js"></script>
+<script type="text/javascript" src="scripts/opened.js"></script>
+<script type="text/javascript" src="scripts/homepage.js"></script>
+</body>
+</html><?php
+
+		wfRestoreWarnings();
+	} // end of execute() method
+}
+
+			
+		
