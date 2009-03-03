b25792682b049e6743974c49f76b90dc782277dd
Asheesh Laroia <asheesh@asheesh.org>
2009-03-03 15:37:42 -0800

    Add legit MIT license header

--- from_zyztzagoo/sbb.js
+++ from_zyztzagoo/sbb.js
@@ -4,6 +4,23 @@
 * @version  0.2
 * @license  http://www.opensource.org/licenses/mit-license.php
 */
+
+/*
+The MIT License
+
+Copyright (c) 2009 zytzagoo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+*/
+
 function Search_Box(cfg) {
     // defaults are always nice
     var defaults = { ELEMENT_ID: 'q', DEFAULT_VALUE: 'inherit', FOCUSED_VALUE: '' };
