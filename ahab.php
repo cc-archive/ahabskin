19557a84409bf7f12bde281feacfa57f0efadd8c
Asheesh Laroia <asheesh@creativecommons.org>
2009-04-07 16:56:25 -0700

    Stop using bundled GIF version of icon; source in the i.creativecommons.org icon

--- ahab.php
+++ ahab.php
@@ -409,7 +409,7 @@ class AhabTemplate extends QuickTemplate {
 ?>			<div id="copyleft">
 				<a href="http://learn.creativecommons.org" id="cclearn"><img src="<?php $this->text('stylepath') ?>/ahab/from_whitewhale/images/common/cclearn_logo.gif" alt="ccLearn" width="92" height="23"/></a>
 				<div id="license">
-					 <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"<img src="<?php $this->text('stylepath') ?>/ahab/from_whitewhale/images/common/license_cc_by.gif" alt="Creative Commons BY License" width="88" height="31"/></a>
+					 <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"<img src="http://i.creativecommons.org/l/by/3.0/us/88x31.png" alt="Creative Commons BY License" width="88" height="31"/></a>
 					<p>Except where otherwise noted, content on this site is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.</p>
 				</div>
 			</div>
