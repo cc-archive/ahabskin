5f5830ab749a9923e55e5b8f8e7cc19090c700da
Alex Kozak <alex@commoner.(none)>
2009-07-24 01:39:33 -0700

    Add Recent Changes (RSS) to footer, change terms link

--- ahab.php
+++ ahab.php
@@ -274,7 +274,8 @@ class AhabTemplate extends QuickTemplate {
 	    <li id="contact">
 	    <a href="http://opened.creativecommons.org/Using_this_Site">Using this site</a> | 
 	    <a href="http://opened.creativecommons.org/About">About this site</a> | 
-	    <a href="http://creativecommons.org/tou">Terms of use</a> | 
+	    <a href="http://opened.creativecommons.org/Special:RecentChanges">Recent Changes</a> (<a href="http://opened.creativecommons.org/index.php?title=Special:RecentChanges&feed=rss">RSS</a>) |
+	    <a href="http://creativecommons.org/terms">Terms of use</a> | 
 	    <a href="http://creativecommons.org/privacy">Privacy policy</a> |
 	    <a href="http://learn.creativecommons.org/contact">Contact us</a>
 	    </li>
