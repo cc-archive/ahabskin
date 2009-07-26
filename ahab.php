81cf100a56d98ffee1cf88e363bf546f755111f5
Alex Kozak <alex@commoner.(none)>
2009-07-26 16:42:00 -0700

    Increase license text size a bit

--- ahab.php
+++ ahab.php
@@ -453,7 +453,7 @@ class AhabTemplate extends QuickTemplate {
                     <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
                     <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a>
 
-<p style="font-size: 80%;">
+<p style="font-size: 90%;">
 Except where otherwise noted, content on this site available
 under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.
 Attribute to <a xmlns:cc="http://creativecommons.org/ns#" href="http://opened.creativecommons.org/" property="cc:attributionName" rel="cc:attributionURL">OpenEd</a>.
